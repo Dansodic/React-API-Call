082681cbf4d3ebb3c94bad1d07b518488244e3e4
dansodic <dankenny360@gmial.com>
2019-08-28 14:46:43 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+//Daniel Kenny 28/08/19
+//Example of how to make a call to an external api with react using componentDidMount
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+          loading : false,
+          character: {}
+        }
+    }
+
+    //Useing componentDidMount to make an external call to an api and resolve the data
+    componentDidMount(){
+      this.setState({loading: true})
+      fetch("https://swapi.co/api/people/4") //Search the Star Wars api for the first person record
+        .then(response => response.json()) // resolve the data
+        .then(data => this.setState({loading:false, character : data})) //set is loading to flase after data resolved
+        
+    }
+
+    render(){
+      //using a ternary operator to display a loading message until the data has been resolved
+      const displayText = this.state.loading ? "Loading..." : this.state.character.name
+      return (
+        <div className="App">
+          <h1>{displayText}</h1>
+          
+        </div>
+      );
+    }
+  
 }
 
 export default App;
